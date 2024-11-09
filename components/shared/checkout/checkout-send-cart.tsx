dfad25189f607ad28f6b5d96e7188398af33a3a6
Alexander Morugin <nobilis@bk.ru>
2024-11-09 16:45:18 +0300

    fix checkout send-form

--- components/shared/checkout/checkout-send-cart.tsx
+++ components/shared/checkout/checkout-send-cart.tsx
@@ -1,28 +1,31 @@
+'use client';
+
 import { type FC } from 'react';
 
 import { CheckoutCartItem } from '..';
+import { useCart } from '@/hooks/use-cart';
 
-type CartItems = {
-  id: number;
-  name: string;
-  quantity: number;
-};
-
-interface Props {
-  cartItems: CartItems[];
-}
-
-export const CheckoutSendCart: FC<Props> = ({ cartItems }) => {
+export const CheckoutSendCart: FC = () => {
+  const { totalAmount, cartItems } = useCart();
   return (
-    <ul className='flex flex-col gap-3'>
-      {cartItems.map((item) => (
-        <CheckoutCartItem
-          key={item.id}
-          name={item.name}
-          quantity={item.quantity}
-          isCheckoutSendCart={true}
-        />
-      ))}
-    </ul>
+    <>
+      <span>Заказ</span>
+      <ul className='flex flex-col gap-3'>
+        {cartItems.map((item) => (
+          <CheckoutCartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            isCheckoutSendCart={true}
+          />
+        ))}
+      </ul>
+      <div className='flex justify-between py-4'>
+        <span>Сумма заказа:</span>
+        <span>
+          <b>{totalAmount}</b> р
+        </span>
+      </div>
+    </>
   );
 };
--- components/shared/checkout/checkout-send-form.tsx
+++ components/shared/checkout/checkout-send-form.tsx
@@ -1,8 +1,5 @@
-'use client';
-
 import { type FC } from 'react';
 import { CheckoutFormValues } from './checkout-form-schema';
-import { useCart } from '@/hooks/use-cart';
 import { CheckoutSendCart, CheckoutSendPersonalForm } from '.';
 
 interface Props {
@@ -10,18 +7,9 @@ interface Props {
 }
 
 export const СheckoutSendForm: FC<Props> = ({ data }) => {
-  const { totalAmount, cartItems } = useCart();
   return (
     <>
-      <span>Заказ</span>
-      <CheckoutSendCart cartItems={cartItems} />
-      <div className='flex justify-between py-4'>
-        <span>Сумма заказа:</span>
-        <span>
-          <b>{totalAmount}</b> р
-        </span>
-      </div>
-
+      <CheckoutSendCart />
       <CheckoutSendPersonalForm data={data} />
     </>
   );
