4233a552e86addd1a5c8cd8ea3f7a5032cdaa32c
Alexander Morugin <nobilis@bk.ru>
2025-03-17 15:12:18 +0300

    made public images

--- prisma/products.ts
+++ prisma/products.ts
@@ -6,8 +6,10 @@ export const products = [
     description:
       'Воздушное белоснежное суфле, приготовленное из сгущенного молока, сахара, сливочного масла, агар-агара (растительного желатина) и лимонной кислоты. Располагается десерт на нежном бисквите и сверху глазирован темным бельгийским горьким шоколадом.',
     price: 1376,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-ptichie-moloko-m-new.fe22f0c3.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-ptichie-moloko-m-new.fe22f0c3.jpeg',
+
+      imageUrl:  `${process.env.BASE_URL}/cake-ptichie-moloko-l-new.webp`,
     categoryId: 1,
   },
   {
@@ -16,8 +18,10 @@ export const products = [
     description:
       'Широко известное пирожное со времён СССР. Приготовлено из шоколадных бисквитных коржей в меру пропитанных сахарным сиропом и кремом, состоящего из сгущенного молока без растительных жиров и какао. Сверху десерт покрыт прослойкой из абрикосового джема и темного шоколада.',
     price: 1090,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-praga-m-new.e154745f.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-praga-m-new.e154745f.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-praga-l-new.webp`,
     categoryId: 1,
   },
   {
@@ -26,8 +30,10 @@ export const products = [
     description:
       'Пирожное из бисквита, отпечённого из пшеничной муки, масла и маковых зёрнышек. Коржи хорошо пропитаны сливочным кремом с добавлением яичных желтков и сахара. Сверху десерт покрыт темной зеркальной шоколадной глазурью и украшен капельками белого шоколада. Отмечено, что насыщенный вкус и аромат мака прекрасно сочетается с шоколадными нотками.',
     price: 1240,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-makoviy-m-new.a7d7cfcb.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-makoviy-m-new.a7d7cfcb.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-makoviy-l-new.webp`,
     categoryId: 1,
   },
   {
@@ -36,8 +42,10 @@ export const products = [
     description:
       'Сливочный мусс на основе темного шоколада, дополненный шоколадным бисквитом',
     price: 1080,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-trufelniy-m-new.287f7713.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-trufelniy-m-new.287f7713.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-trufelniy-l-new.webp`,
     categoryId: 1,
   },
 
@@ -48,8 +56,10 @@ export const products = [
     description:
       'Торт представляет собой довольно необычный десерт, состоящий из воздушного и мягкого бисквита красного цвета. Оригинальность данному изделию придает и белоснежный сливочный крем',
     price: 1160,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-barkhat-m-new.18887871.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-barkhat-m-new.18887871.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-barkhat-l-new.webp`,
     categoryId: 2,
   },
   {
@@ -58,8 +68,10 @@ export const products = [
     description:
       'Лимонный торт выполнен из трёх бисквитных коржей, между которыми выложен белоснежный творожно-сливочный нежный крем. Торт не рыхлый из-за малой пропитки коржей. Вкус торта очень яркий, сливочно-лимонный, освежающий и не слишком сладкий.',
     price: 1140,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-lemon-m-new.74869bd3.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-lemon-m-new.74869bd3.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-lemon-l-new.webp`,
     categoryId: 2,
   },
   {
@@ -68,8 +80,10 @@ export const products = [
     description:
       'Яркий медовый торт, будоражащий самые притязательные вкусы сладкоежек. Очень и очень мягкий, нежный, бисквитно кремовый. Готовится из трех коржей оттенка молочного шоколада, сильно пропитанных мёдом и сдобренных воздушным сливочно-творожным кремом.',
     price: 940,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-med-moloko-m-new.2a2d19ca.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-med-moloko-m-new.2a2d19ca.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-med-moloko-l-new.webp`,
     categoryId: 2,
   },
   {
@@ -78,8 +92,10 @@ export const products = [
     description:
       'Вкусный фруктовый торт приготовлен из трех бисквитных коржей тёмного шоколадного цвета, прослоенных сливочным кремом с кусочками банана. Нижний корж сухой, не пропитанный и крепкий. Остальные коржи очень сочные и мягкие. Между нижним и средним коржами выложен белый нежный крем, поверх которого расположен банан. Между средним и верхним коржами используется другой крем. Он более вязкий и имеет цвет крем-брюле.',
     price: 1190,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-banana-m-new.ef129319.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-banana-m-new.ef129319.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-banana-l-new.webp`,
     categoryId: 2,
   },
 
@@ -90,8 +106,10 @@ export const products = [
     description:
       'Необыкновенно нежный и вкусный торт, приготовленный из трёх коржей шоколадного бисквита пропитанных мёдом. Коржи прослоены очень мягким творожно-сливочным кремом сбалансированным вареным сгущенным молоком. На нижний корж торта выкладывается чуть соленый арахис в окружении тягучей карамели. Данный торт является одним из лучших произведений нашей кондитерской! Настоятельно рекомендуем к покупке.',
     price: 900,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-orekhoviy-m-new.979a42b7.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-orekhoviy-m-new.979a42b7.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-orekhoviy-l-new.webp`,
     categoryId: 3,
   },
   {
@@ -100,8 +118,10 @@ export const products = [
     description:
       'Слоенное тесто на основе сгущенного молока, пропитан кремом из сливочного сыра. Упакован в цветную картонную коробку с окошком',
     price: 1150,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-vozdushno-m-new.260d801c.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-vozdushno-m-new.260d801c.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-vozdushno-l-new.webp`,
     categoryId: 3,
   },
   {
@@ -110,8 +130,10 @@ export const products = [
     description:
       'Весьма известный венгерский ореховый торт, приготовленный из нескольких песочных коржей с прослойкой из заварного крема, ореховой пасты и сгущенного молока. Сверху торт покрыт клубничным джемом собственного производства и глазурью из белого шоколада.',
     price: 1280,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-esterhazi-m-new.bb6c3570.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-esterhazi-m-new.bb6c3570.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-esterhazi-l-new.webp`,
     categoryId: 3,
   },
   {
@@ -120,8 +142,10 @@ export const products = [
     description:
       'Вкусный сметанный торт приготовлен из четырёх мягких бисквитов песочного цвета. Между сухими (не пропитанными) коржами выложен белый сливочно-сырный крем очень нежной ароматной консистенции. Торт наполнен большим количеством дробленых грецких орехов.',
     price: 1170,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-smetannik-m-new.b7a5280e.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-smetannik-m-new.b7a5280e.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-smetannik-l-new.webp`,
     categoryId: 3,
   },
 
@@ -132,8 +156,10 @@ export const products = [
     description:
       'Известный торт приготовленный из нежных хрустящих ореховых коржей, которые выпекаются с добавлением муки, сахара и яичных белков. Эти коржи, подобные "Безе", прослоены немного тягучим сливочным кремом, приготовленным из сгущенного молока и орехов. Сверху торт украшен сахарной пудрой и грецкими орехами.',
     price: 940,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-kievskiy-m-new.7366e564.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-kievskiy-m-new.7366e564.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-kievskiy-l-new.webp`,
     categoryId: 4,
   },
   {
@@ -142,8 +168,10 @@ export const products = [
     description:
       'Изумительно нежный и мягкий торт из хрустящего слоеного теста и нежного сливочного крема. Пышный, сочный, тающий во рту. Состоит из четырех слоев теста и крема из яичных желтков, сыра "Маскарпоне", кондитерских густых сливок, с добавлением желатина, ванильного сахара и сливочного масла. Сверху торт украшен роскошной шапкой из крошки слоеного теста и сахарной пудры.',
     price: 1200,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-napoleon-m-new.e7508136.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-napoleon-m-new.e7508136.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-napoleon-l-new.webp`,
     categoryId: 4,
   },
   {
@@ -152,8 +180,10 @@ export const products = [
     description:
       'Из нежных песочных коржей пропитанных медом и легким ванильным кремом',
     price: 1070,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-medoviy-m-new.ecea3c7f.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-medoviy-m-new.ecea3c7f.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-medoviy-l-new.webp`,
     categoryId: 4,
   },
   {
@@ -162,8 +192,10 @@ export const products = [
     description:
       'Торт состоит из двух коржей, выпеченных их пшеничной и миндальной муки. В начинке, расположенной между коржами, открывается необычное сочетание миндаля с черносливом и грецким орехом совместно с нежным сливочным кремом, что придает торту незабываемый вкус.',
     price: 1160,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-mindal-m-new.00550606.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-mindal-m-new.00550606.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-mindal-l-new.webp`,
     categoryId: 4,
   },
 
@@ -174,8 +206,10 @@ export const products = [
     description:
       'Классический рецепт. Испечен из нежного творожного сыра, на тонком слое бисквита',
     price: 1250,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-cheescake-m-new.0e5af8b0.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-cheescake-m-new.0e5af8b0.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-cheescake-l-new.webp`,
     categoryId: 5,
   },
   {
@@ -184,8 +218,10 @@ export const products = [
     description:
       'Пирожное из черничного мусса на песочном ложе. Основа готовится из свежемороженных ягод, белого шоколада, густых сливок, сахара и желатина. Корж отпекается из пшеничной и миндальной муки, с добавлением мёда и сливочного масла. Сверху десерт украшен нейтральным кондитерским гелем.',
     price: 1220,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-chernichniy-m-new.c80d3743.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-chernichniy-m-new.c80d3743.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-chernichniy-l-new.webp`,
     categoryId: 5,
   },
   {
@@ -194,8 +230,10 @@ export const products = [
     description:
       'Торт с едва уловимым вкусом моркови, с добавлением грецкого ореха, изюма, творожного сыра и корицы',
     price: 1180,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-morkovniy-m-new.1f34ec3a.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-morkovniy-m-new.1f34ec3a.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-morkovniy-l-new.webp`,
     categoryId: 5,
   },
   {
@@ -204,8 +242,10 @@ export const products = [
     description:
       'Очень нежный торт, приготовленный из легкого фруктово-сливочного мусса расположенного на основании из песочно-медового коржа. Элегантный, тающий во рту, возбуждающий своим вкусом, способен понравится любителям не слишком сладких и вязких десертов.',
     price: 1260,
-    imageUrl:
-      'https://pralinecakes.ru/_next/static/media/cake-lime-m-new.113aa230.jpeg',
+    // imageUrl:
+    //   'https://pralinecakes.ru/_next/static/media/cake-lime-m-new.113aa230.jpeg',
+
+    imageUrl:  `${process.env.BASE_URL}/cake-lime-l-new.webp`,
     categoryId: 5,
   },
 ];
